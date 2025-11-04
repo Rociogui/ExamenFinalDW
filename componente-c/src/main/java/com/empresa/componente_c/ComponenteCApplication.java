6e827acf153df7bae949e9b21a98109a16462e93
Rociogui <rlopeza19@miumg.edu.gt>
2025-11-03 21:51:35 -0600

    commit2 crear componente-c utils

---
+++ componente-c/src/main/java/com/empresa/componente_c/ComponenteCApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.componente_c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponenteCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponenteCApplication.class, args);
+	}
+
+}
---
+++ componente-c/src/test/java/com/empresa/componente_c/ComponenteCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.componente_c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponenteCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
