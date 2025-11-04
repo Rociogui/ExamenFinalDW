5a070ab77b1e3a3da6bb64fa925a441ca0464d9b
Rociogui <rlopeza19@miumg.edu.gt>
2025-11-04 13:53:14 -0600

    commit3 configurar mariadb

---
+++ componente-a/src/test/java/com/empresa/a/componente_a/ComponenteAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.a.componente_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponenteAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
