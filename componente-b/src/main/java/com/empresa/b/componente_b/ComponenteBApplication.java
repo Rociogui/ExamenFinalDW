41537ba3916b733c2cd59781b3489a2cf32f9eb2
Rociogui <rlopeza19@miumg.edu.gt>
2025-11-11 12:09:54 -0600

    commit9 crear componente B base con PostgreSQL

---
+++ componente-b/src/main/java/com/empresa/b/componente_b/ComponenteBApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.b.componente_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponenteBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponenteBApplication.class, args);
+	}
+
+}
